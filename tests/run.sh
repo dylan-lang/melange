f97f8b3e1b9bc44b464c67aaf194d1a8835dc38d
Bruce Mitchener <bruce.mitchener@gmail.com>
2015-10-07 21:29:53 +0700

    [tests] Support testing multiple back-ends.
    This makes the script check for the expected C-FFI output. Soon,
    we'll be checking against the expected mindy output as well.
    
    * tests/run.sh: Check against expected-c-ffi.dylan and rename some
      variables to indicate that they're back-end specific.
    
    * tests/*/expected.dylan: Rename to expected-c-ffi.dylan

--- tests/run.sh
+++ tests/run.sh
@@ -3,29 +3,29 @@
 for directory in test.*; do
     [ -d "${directory}" ] || continue
     input="${directory}/test.intr"
-    expected="${directory}/expected.dylan"
-    actual="${directory}/actual.dylan"
+    expected_c_ffi="${directory}/expected-c-ffi.dylan"
+    actual_c_ffi="${directory}/actual-c-ffi.dylan"
 
     if [ ! -f "$input" ]; then
       printf "Input file %s is missing.\n" "$input"
       exit 1;
     fi
-    if [ ! -f "$expected" ]; then
-      printf "Expected output file %s is missing.\n" "$expected"
+    if [ ! -f "$expected_c_ffi" ]; then
+      printf "Expected output file %s is missing.\n" "$expected_c_ffi"
       exit 1;
     fi
 
-    ../_build/bin/melange -I${directory} -Tc-ffi $input $actual
+    ../_build/bin/melange -I${directory} -Tc-ffi $input $actual_c_ffi
 
-    diff -u $expected $actual
+    diff -u $expected_c_ffi $actual_c_ffi
 
     if [ $? != 0 ]; then
       printf "Test failed: %s\n" "${directory}";
-      rm $actual;
+      rm $actual_c_ffi;
       exit 1;
     else
       printf "Test OK: %s\n" "${directory}";
-      rm $actual;
+      rm $actual_c_ffi;
     fi
 done
 
